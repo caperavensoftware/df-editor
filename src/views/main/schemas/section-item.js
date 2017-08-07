adff4561167deaa3f3bf100e74c41add444bfcb2
caperaven@icloud.com <caperaven@icloud.com>
2017-08-07 17:28:07 +0200

    initial

--- src/views/main/schemas/section-item.js
+++ src/views/main/schemas/section-item.js
@@ -1,41 +1,102 @@
 export const template =
 {
-  "type": "view",
-  "fields": [
-      {
+    "type": "view",
+    "fields": [
+        {
         "field": "id",
         "map": "id"
-      }
-  ],
-  "body": {
-    "elements": [
+        }
+    ],
+    "datasources": [
         {
-            "element": "group",
-            "title": "Section Item",
-            "elements": [
+            "id": 0,
+            "resource": [
                 {
-                    "element": "input",
-                    "title": "Id",
-                    "field": "id"
+                    "id": 0,
+                    "title": "text"
                 },
                 {
-                    "element": "input",
-                    "title": "Name",
-                    "field": "name"
-                }
-            ]
-        },
-        {
-            "element": "card",
-            "elements": [
+                    "id": 1,
+                    "title": "boolean"
+                },
                 {
-                    "element": "button",
-                    "title": "Back",
-                    "action": "context.propCrumb",
-                    "styles": ["back"]
+                    "id": 2,
+                    "title": "date"
+                },
+                {
+                    "id": 3,
+                    "title": "time"
+                },
+                {
+                    "id": 4,
+                    "title": "number"
+                },
+                {
+                    "id": 5,
+                    "title": "memo"
+                },
+                {
+                    "id": 6,
+                    "title": "label"
+                },
+                {
+                    "id": 7,
+                    "title": "group"
+                },
+                {
+                    "id": 8,
+                    "title": "endgroup"
+                },
+                {
+                    "id": 9,
+                    "title": "heading"
+                },
+                {
+                    "id": 10,
+                    "title": "detail"
+                },
+                {
+                    "id": 11,
+                    "title": "conditional"
                 }
             ]
         }
-    ]
-  }
+    ],
+    "body": {
+        "elements": [
+            {
+                "element": "group",
+                "title": "Section Item",
+                "elements": [
+                    {
+                        "element": "input",
+                        "title": "Id",
+                        "field": "id"
+                    },
+                    {
+                        "element": "input",
+                        "title": "Name",
+                        "field": "name"
+                    },
+                    {
+                        "element": "select",
+                        "datasource": 0,
+                        "title": "Type",
+                        "field": "datatype"
+                    }
+                ]
+            },
+            {
+            "element": "card",
+                "elements": [
+                    {
+                        "element": "button",
+                        "title": "Back",
+                        "action": "context.propCrumb",
+                        "styles": ["back"]
+                    }
+                ]
+            }
+        ]
+    }
 };
